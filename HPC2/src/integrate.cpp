b57299a6fbe17affee192908a86a91f014f1e0e2
Max Revitt <max.revitt@gmail.com>
2013-03-09 19:29:58 +0000

    init code

---
+++ HPC2/src/integrate.cpp
@@ -0,0 +1,14 @@
+#include <CL/cl.hpp>
+#include <tbb/tbb.h>
+#include <iostream>
+
+// I think the idea is we rewrite these
+#include "functions.hpp";
+// In here, making use of OpenCL
+#include "integrate.hpp";
+
+int main(int argc, char **argv){
+	std::cout << "hello world" << std::endl;
+	std::cin.get();
+	return 0;
+}
---
+++ HPC2/src/integrate.hpp
@@ -0,0 +1,15 @@
+#ifndef INTEGRATE_HPP_
+#define INTEGRATE_HPP_
+
+#define __CL_ENABLE_EXCEPTIONS
+#define __NO_STD_VECTOR // Use cl::vector instead of STL version
+
+
+// functions for integration using OPEN CL!!!
+
+#include <CL/cl.hpp>
+
+
+
+
+#endif
