1b29dbb398d333bc3797aefb0e8691a575d7fdea
Max Revitt <max.revitt@gmail.com>
2013-03-11 18:07:11 +0000

    removed namespacing

--- HPC2/src/integrate.cpp
+++ HPC2/src/integrate.cpp
@@ -1,3 +1,4 @@
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS  // My gfx card only supports 1.1
 #define __NO_STD_VECTOR // Use cl::vector instead of STL version
 #define __CL_ENABLE_EXCEPTIONS
 
@@ -15,58 +16,58 @@
 
 #include "Timer.hpp";
 
+using namespace cl;
+
 int main(int argc, char **argv){
 
-	using namespace cl;
-
-// Create the two input vectors
+	// Create the two input vectors
     const int LIST_SIZE = 1000;
-    int *A = new int[LIST_SIZE]; 
+    int *A = new int[LIST_SIZE];
     int *B = new int[LIST_SIZE];
     for(int i = 0; i < LIST_SIZE; i++) {
         A[i] = i;
         B[i] = LIST_SIZE - i;
     }
  
-   try { 
+   try {
         // Get available platforms
-        vector<Platform> platforms;
-        Platform::get(&platforms);
+        cl::vector<cl::Platform> platforms;
+        cl::Platform::get(&platforms);
  
         // Select the default platform and create a context using this platform and the GPU
-        cl_context_properties cps[3] = { 
-            CL_CONTEXT_PLATFORM, 
-            (cl_context_properties)(platforms[0])(), 
-            0 
+        cl_context_properties cps[3] = {
+            CL_CONTEXT_PLATFORM,
+            (cl_context_properties)(platforms[0])(),
+            0
         };
-        Context context( CL_DEVICE_TYPE_GPU, cps);
+        cl::Context context(CL_DEVICE_TYPE_GPU, cps);
  
         // Get a list of devices on this platform
-        vector<Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+        cl::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
  
         // Create a command queue and use the first device
-        CommandQueue queue = CommandQueue(context, devices[0]);
+        cl::CommandQueue queue = cl::CommandQueue(context, devices[0]);
  
         // Read source file
         std::ifstream sourceFile("..\\HPC2\\src\\vector_add_kernel.cl");
         std::string sourceCode(
             std::istreambuf_iterator<char>(sourceFile),
             (std::istreambuf_iterator<char>()));
-        Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length()+1));
+        cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length()+1));
  
         // Make program of the source code in the context
-        Program program = Program(context, source);
+        cl::Program program = cl::Program(context, source);
  
         // Build program for these specific devices
         program.build(devices);
  
         // Make kernel
-        Kernel kernel(program, "vector_add");
+        cl::Kernel kernel(program, "vector_add");
  
         // Create memory buffers
-        Buffer bufferA = Buffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int));
-        Buffer bufferB = Buffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int));
-        Buffer bufferC = Buffer(context, CL_MEM_WRITE_ONLY, LIST_SIZE * sizeof(int));
+        cl::Buffer bufferA = cl::Buffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int));
+        cl::Buffer bufferB = cl::Buffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int));
+        cl::Buffer bufferC = cl::Buffer(context, CL_MEM_WRITE_ONLY, LIST_SIZE * sizeof(int));
  
         // Copy lists A and B to the memory buffers
         queue.enqueueWriteBuffer(bufferA, CL_TRUE, 0, LIST_SIZE * sizeof(int), A);
@@ -78,21 +79,20 @@ int main(int argc, char **argv){
         kernel.setArg(2, bufferC);
  
         // Run the kernel on specific ND range
-        NDRange global(LIST_SIZE);
-        NDRange local(1);
-        queue.enqueueNDRangeKernel(kernel, NullRange, global, local);
+        cl::NDRange global(LIST_SIZE);
+        cl::NDRange local(1);
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, global, local);
  
         // Read buffer C into a local list
         int *C = new int[LIST_SIZE];
         queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, LIST_SIZE * sizeof(int), C);
  
         for(int i = 0; i < LIST_SIZE; i ++)
-             std::cout << A[i] << " + " << B[i] << " = " << C[i] << std::endl; 
-    } catch(Error error) {
+             std::cout << A[i] << " + " << B[i] << " = " << C[i] << std::endl;
+    } catch(cl::Error error) {
        std::cout << error.what() << "(" << error.err() << ")" << std::endl;
     }
-   
+
    std::cin.get();
- 
-    return 0;
+   return 0;
 }
